7707fe68fa4c559e582b7e32a40f3763af768bab
ganarandhi <gana.randhi@gmail.com>
2024-09-07 18:47:06 -0400

    Added Continuation of Project

--- MyPoki.Repository/obj/Release/net8.0/MyPoki.Repository.AssemblyInfo.cs
+++ MyPoki.Repository/obj/Release/net8.0/MyPoki.Repository.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPoki.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+103c348a200a8a947c5fe54d3863d52e09a642a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37ec02e184a1381163bf3dadbc7acadfca116d4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPoki.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPoki.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MyPokiApp.ConsoleApp/RunPoki.cs
+++ MyPokiApp.ConsoleApp/RunPoki.cs
@@ -7,81 +7,84 @@ class RunPoki
     {
         Console.WriteLine("Hello From Poki World! Please Enter your Name:");
         var playerName = Console.ReadLine();
-        MyPokeApiClient pokeClient = new MyPokeApiClient();
-        Pokemon pokemonPage = new();
-        string pokiName = "";       
-        
-        bool isEntryGood = false;
-        do
+        bool continueApp = true;
+        while (continueApp)
         {
-            Console.WriteLine(playerName + " Please Enter Pokemon Name:");
-            pokiName = Console.ReadLine();
-            if(String.IsNullOrEmpty(pokiName) || String.IsNullOrWhiteSpace(pokiName))
+            MyPokeApiClient pokeClient = new MyPokeApiClient();
+            Pokemon pokemonPage = new();
+            string pokiName = "";       
+            
+            bool isEntryGood = false;
+            do
             {
-                Console.WriteLine("You sure you enter a name??");
-            }
-            else 
-                isEntryGood = true;
-        } while (!isEntryGood);
+                Console.WriteLine(playerName + " Please Enter Pokemon Name:");
+                pokiName = Console.ReadLine();
+                if(String.IsNullOrEmpty(pokiName) || String.IsNullOrWhiteSpace(pokiName))
+                {
+                    Console.WriteLine("You sure you enter a name??");
+                }
+                else 
+                    isEntryGood = true;
+            } while (!isEntryGood);
         
-        try
-        {            
-            pokemonPage = await pokeClient.GetResourceAsync<Pokemon>(pokiName);
+            try
+            {            
+                pokemonPage = await pokeClient.GetResourceAsync<Pokemon>(pokiName);
 
-        }
-        catch (Exception)
-        {
-            Console.WriteLine("The " + pokiName + " entered is Not Available.");
-            Console.WriteLine("Or Try a different pokemon next time.\n Please press Enter to exit");
-            Console.ReadLine();
-            Environment.Exit(-1);
-        }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The " + pokiName + " entered is Not Available.");
+            }
+            
+            try
+            {
+                Console.WriteLine("Thats an interesting Pokemon name ");
 
-        try
-        {
-            Console.WriteLine("Thats an interesting Pokemon name ");
-
-            // Get the first page of Pokémon (default limit is 20)
-            var pokeAbility = await pokeClient.GetResourceAsync<Ability>(pokemonPage.Id.ToString());
+                // Get the first page of Pokémon (default limit is 20)
+                var pokeAbility = await pokeClient.GetResourceAsync<Ability>(pokemonPage.Id.ToString());
                 
-            string types = string.Join(", ", pokemonPage.Types.Select(x=>x.Type.Name));
+                string types = string.Join(", ", pokemonPage.Types.Select(x=>x.Type.Name));
 
-            //Print Pokemon Types, Resource.
-            Console.WriteLine("Your "+pokiName +" has Types : " + types);
-            Console.WriteLine("\nYour "+pokiName +"'s Resource Name: "+ pokeAbility.Name);
+                //Print Pokemon Types, Resource.
+                Console.WriteLine("Your "+pokiName +" has Types : " + types);
+                Console.WriteLine("\nYour "+pokiName +"'s Resource Name: "+ pokeAbility.Name);
 
-            //Print Abilities
-            string ability = string.Join(", ",pokemonPage.Abilities.Select(x=>x.Ability.Name));
-            Console.WriteLine("Your "+pokiName +"'s Abilities are: "+ ability);
-            Console.WriteLine("Your "+pokiName +"'s strengths and weaknesses are: "+pokeAbility.EffectEntries[1].Effect.ToString());   
+                //Print Abilities
+                string ability = string.Join(", ",pokemonPage.Abilities.Select(x=>x.Ability.Name));
+                Console.WriteLine("Your "+pokiName +"'s Abilities are: "+ ability);
+                Console.WriteLine("Your "+pokiName +"'s strengths and weaknesses are: "+pokeAbility.EffectEntries[1].Effect.ToString());   
              
-        }
-        catch (Exception)
-        {            
-            Console.WriteLine("The " + pokiName + " entered is still in an Egg and yet to born. \n Please try after few more months. It needs time to HATCH, PATCH and CATCH.");
-            Console.WriteLine("\n Please press Enter to exit");
-            Console.ReadLine();
-            Environment.Exit(-1);
-        } 
-        //check Damage Stats
-        try
-        {
-            var pokeTypes = await pokeClient.GetResourceAsync<MyPoki.Repository.Models.Type>(pokemonPage.Id);
-            //Strong or Weak
-            string strongAgainst = GetStrongTo(pokeTypes);
-            string weakAgainst = GetWeakTo(pokeTypes);
+            }
+            catch (Exception)
+            {            
+                Console.WriteLine("The " + pokiName + " entered is still in an Egg and yet to born. \n Please try after few more months. It needs time to HATCH, PATCH and CATCH.");
                 
-            Console.WriteLine("\nYour "+pokiName +"'s Stronger against : \n" + strongAgainst);
-            Console.WriteLine("\nYour "+pokiName +"'s Seaker against : \n" + weakAgainst);
-            Console.WriteLine("\n Please press Enter to exit");
-            Console.ReadLine();
+            }
+             
+            //check Damage Stats
+            try
+            {
+                var pokeTypes = await pokeClient.GetResourceAsync<MyPoki.Repository.Models.Type>(pokemonPage.Id);
+                //Strong or Weak
+                string strongAgainst = GetStrongTo(pokeTypes);
+                string weakAgainst = GetWeakTo(pokeTypes);
+                
+                Console.WriteLine("\nYour "+pokiName +"'s Stronger against : \n" + strongAgainst);
+                Console.WriteLine("\nYour "+pokiName +"'s Seaker against : \n" + weakAgainst);
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("\nThe " + pokiName + " has issues pulling the damage i.e., Attack and Defence details. \n Please try after few more months. It needs time to HATCH, PATCH and CATCH.\n\nOr try Pokemon Name: Bulbasaur/Ivysaur/Venusaur");                
+            }
+            
+                Console.WriteLine("\n Do you want to enter new Pokemon? (Y/N)");
+                string userInput = Console.ReadLine();
+                if (userInput.ToLower() != "y")
+                {
+                    continueApp = false;
+                }
         }
-        catch (System.Exception)
-        {
-            Console.WriteLine("\nThe " + pokiName + " has issues pulling the damage i.e., Attack and Defence details. \n Please try after few more months. It needs time to HATCH, PATCH and CATCH.\n\nOr try Pokemon Name: Bulbasaur/Ivysaur/Venusaur");
-            Console.WriteLine("\n Please press Enter to exit");
-            Console.ReadLine();
-         }
     }
 
     private static string GetStrongTo(MyPoki.Repository.Models.Type pokeTypes)
